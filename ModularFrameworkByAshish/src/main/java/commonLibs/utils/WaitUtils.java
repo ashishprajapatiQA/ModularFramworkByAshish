23a0dd7f4b16644dbb86b840d0f3aaba39d5f1aa
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-03-25 11:18:11 +0530

    interact with iframes

--- ModularFrameworkByAshish/src/main/java/commonLibs/utils/WaitUtils.java
+++ ModularFrameworkByAshish/src/main/java/commonLibs/utils/WaitUtils.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtils {
-	public static void waitTillAlertIsPresent(WebDriver driver, int timeoutInSeconds) throws Exception{
+	public static void waitTillAlertIsPresent(WebDriver driver, int timeoutInSeconds) throws Exception {
 		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
 		wait.until(ExpectedConditions.alertIsPresent());
 	}
