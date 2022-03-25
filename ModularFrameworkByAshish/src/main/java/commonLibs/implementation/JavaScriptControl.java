be48e954f8341a6607d3c1e235f80d1811e375e0
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-03-25 11:52:34 +0530

    javascriptExecution controller is pending

---
+++ ModularFrameworkByAshish/src/main/java/commonLibs/implementation/JavaScriptControl.java
@@ -0,0 +1,25 @@
+package commonLibs.implementation;
+
+import commonLibs.contracts.IJavaScriptOperations;
+
+public class JavaScriptControl implements IJavaScriptOperations{
+
+	@Override
+	public void executeJavaScript(String scriptToExecute) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scrollDown(int x, int y) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void executeJavaScriptWithReturnValue(String scriptToExecute) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
