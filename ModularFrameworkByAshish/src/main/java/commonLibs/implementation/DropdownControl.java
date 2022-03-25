ce8e25967cae38b8941b566842a7af6b5c75d210
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-03-25 10:15:51 +0530

    Interacting with Dropdown

---
+++ ModularFrameworkByAshish/src/main/java/commonLibs/implementation/DropdownControl.java
@@ -0,0 +1,42 @@
+package commonLibs.implementation;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import commonLibs.contracts.IDropdown;
+
+public class DropdownControl implements IDropdown {
+
+	private Select getSelect(WebElement element) {
+		Select select = new Select(element);
+		return select;
+	}
+
+	@Override
+	public void selectViaVisibleText(WebElement element, String visibleText) throws Exception {
+		// TODO Auto-generated method stub
+
+		getSelect(element).selectByVisibleText(visibleText);
+	}
+
+	@Override
+	public void selectViaValue(WebElement element, String value) throws Exception {
+		// TODO Auto-generated method stub
+		getSelect(element).selectByValue(value);
+	}
+
+	@Override
+	public void selectViaIndex(WebElement element, int index) throws Exception {
+		// TODO Auto-generated method stub
+		getSelect(element).selectByIndex(index);
+	}
+
+	@Override
+	public List<WebElement> getAllOption(WebElement element) {
+		// TODO Auto-generated method stub
+		return getSelect(element).getOptions();
+	}
+
+}
