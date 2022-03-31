361fb325150a20170991df61c9db23ee2aaa95c0
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-03-31 14:13:59 +0530

    wait Utils

--- ModularFrameworkByAshish/src/main/java/commonLibs/utils/WaitUtils.java
+++ ModularFrameworkByAshish/src/main/java/commonLibs/utils/WaitUtils.java
@@ -1,6 +1,7 @@
 package commonLibs.utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -8,5 +9,22 @@ public class WaitUtils {
 	public static void waitTillAlertIsPresent(WebDriver driver, int timeoutInSeconds) throws Exception {
 		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
 		wait.until(ExpectedConditions.alertIsPresent());
+	}
+
+	public static void waitTillElementVisiable(WebDriver driver, WebElement element, int timeoutInSeconds)
+			throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public static void waitTillElementClickable(WebDriver driver, WebElement element, int timeoutInSeconds)
+			throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	public static void waitTillElementSelectable(WebDriver driver, WebElement element, int timeoutInSeconds)
+			throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+		wait.until(ExpectedConditions.elementToBeSelected(element));
 	}
 }
