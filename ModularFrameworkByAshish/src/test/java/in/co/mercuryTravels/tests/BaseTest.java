dba6e363fc65b664321d4318f5a9d198f56d60fb
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-05-18 14:58:05 +0530

    config file 1st commit

--- ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/BaseTest.java
+++ ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/BaseTest.java
@@ -1,10 +1,13 @@
 package in.co.mercuryTravels.tests;
 
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import commonLibs.implementation.CommonDriver;
+import commonLibs.utils.ConfigFileUtils;
 import in.co.mercuryTravels.pages.HomePage;
 
 public class BaseTest {
@@ -12,13 +15,26 @@ public class BaseTest {
 	// Initialize home page like basic thing driver, url,browser type , open/close browser  
 	
 	CommonDriver cmnDriver;
-	String browserType = "chrome";
+	String browserType;
 	String url = "https://www.mercurytravels.co.in/";
 	HomePage homePage;
 	private WebDriver driver;
+	
+	static String configFileName;
+	static Properties configProperties;
+	static {
+		try {
+			configProperties = ConfigFileUtils.readProperties(configFileName);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 	@BeforeClass
 	public void invokeBrowser() throws Exception {
+		browserType = configProperties.getProperty("browserType");
 		cmnDriver = new CommonDriver(browserType);
 		cmnDriver.setPageloadTimeout(60);
 		cmnDriver.setElementDetectionTimeout(1000000);
