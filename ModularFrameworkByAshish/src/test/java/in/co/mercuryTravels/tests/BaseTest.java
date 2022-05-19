8a2c1a3c2872295f324ef4d4026da0d3db7faf0c
RAjdip14 <rajdip.shethana9202@gmail.com.com>
2022-05-19 12:02:46 +0530

    Extend reports for loginuser is Done

--- ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/BaseTest.java
+++ ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/BaseTest.java
@@ -4,61 +4,103 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import commonLibs.implementation.CommonDriver;
 import commonLibs.utils.ConfigFileUtils;
+import commonLibs.utils.DateUtils;
 import in.co.mercuryTravels.pages.HomePage;
 
 public class BaseTest {
-	
-	// Initialize home page like basic thing driver, url,browser type , open/close browser  
-	
+
+	// Initialize home page like basic thing driver, url,browser type , open/close
+	// browser
+
 	CommonDriver cmnDriver;
 	String browserType;
 	String baseUrl;
 	HomePage homePage;
 	private WebDriver driver;
-	
+
+	// configuration file data fetch
 	static String configFileName;
 	static Properties configProperties;
 	static String currentWorkingDirectory;
-	
+	static String executionStartDate;
+
 	int pageloadtimeout;
 	int elementDetectionTimeout;
+
+	// reports
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+	ExtentTest extentTest;
+
+	String reportFilename;
+
 	static {
-		
-	
+
 		try {
 			currentWorkingDirectory = System.getProperty("user.dir");
-			configFileName =  String.format("%s/config/config.properties",currentWorkingDirectory);
+			executionStartDate = DateUtils.getCurrentDateAndTime();
+			configFileName = String.format("%s/config/config.properties", currentWorkingDirectory);
 			configProperties = ConfigFileUtils.readProperties(configFileName);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+	}
+
+	@BeforeSuite
+	public void preSetup() {
+		reportFilename = String.format("%s/reports/MercuryTravelTestReport-%s.html", currentWorkingDirectory,
+				executionStartDate);
+		htmlReporter = new ExtentHtmlReporter(reportFilename);
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
 	}
 
 	@BeforeClass
-	public void invokeBrowser() throws Exception {
+	public void setup() throws Exception {
+		extentTest = extent.createTest("Setup = set up the pre-requiesit to run automated test cases");
+
 		browserType = configProperties.getProperty("browserType");
+		extentTest.log(Status.INFO, "Browser invoked is " + browserType); // report log
 		cmnDriver = new CommonDriver(browserType);
-		
+
 		pageloadtimeout = Integer.parseInt(configProperties.getProperty("pageloadTimeout"));
+		extentTest.log(Status.INFO, "Page Load Timeout is " + pageloadtimeout); // report log
 		elementDetectionTimeout = Integer.parseInt(configProperties.getProperty("elementDetectionTimeout"));
-		cmnDriver.setPageloadTimeout(pageloadtimeout);		
+		extentTest.log(Status.INFO, "Implicit wait set is " + elementDetectionTimeout); // report log
+		cmnDriver.setPageloadTimeout(pageloadtimeout);
 		cmnDriver.setElementDetectionTimeout(elementDetectionTimeout);
-		
+
 		baseUrl = configProperties.getProperty("baseUrl");
+		extentTest.log(Status.INFO, "Base URL where the browser navigates to -" + baseUrl); // report log
 		cmnDriver.navigateToUrl(baseUrl);
 		driver = cmnDriver.getDriver();
+		extentTest.log(Status.INFO, "Initializing all pages"); // report log
 		homePage = new HomePage(driver);
 		homePage.closeInitialButton();
 	}
 
 	@AfterClass
-	public void closeBrowser() throws Exception {
+	public void cleanUp() throws Exception {
 		cmnDriver.closeAllBrowser();
+		extentTest = extent.createTest("Clean up - Clean process");
+		extentTest.log(Status.INFO, "Closing all browser instances"); // report log
 	}
+
+	@AfterSuite
+	public void postCleanUp() {
+		extent.flush();
+	}
+
 }
--- ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/MercuryTravelTestcases.java
+++ ModularFrameworkByAshish/src/test/java/in/co/mercuryTravels/tests/MercuryTravelTestcases.java
@@ -3,17 +3,25 @@ package in.co.mercuryTravels.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 public class MercuryTravelTestcases extends BaseTest{
 // Here is we write the Test cases & pass value to logic layer(here all steps defined just need values) 
 
 	@Test
 	public void verifyUserLoginWithCorrectCredential() throws Exception{
+		
+		extentTest = extent.createTest("TC-001 - verify Userlogin with correct credentials");
+		
 		String username = configProperties.getProperty("userEmailId");
+		extentTest.log(Status.INFO, "User Email-id -" + username);
 		String password = configProperties.getProperty("userPassword");
+		extentTest.log(Status.INFO, "User Password -" + password);
         homePage.userLogin(username,password);// this pass value to logic layer page
         String expectedWelcomeText = configProperties.getProperty("expectedWelcomeText");
         String actualWelcomeText = homePage.getWelcomeMessage();
         Assert.assertEquals(actualWelcomeText, expectedWelcomeText);
+        extentTest.log(Status.INFO, "User Login Successfully");
 	}
 
 }
